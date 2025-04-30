cddf256c57dd3747726d8e839a84a1be184bdbab
Yogesh Kumar <yogeshdevveloper07@gmail.com>
2025-04-30 20:54:26 +0530

    Vinayak project initial commit from local

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
+<<<<<<< HEAD
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
+=======
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+>>>>>>> ff22584c (Initial commit for Vinayak project)
             refresh: true,
         }),
     ],
